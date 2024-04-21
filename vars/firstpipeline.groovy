7d6c06e5bdc2a3dd55a294a1364a3008f6d37d6c
Ananyamooli <161895793+Ananyamooli@users.noreply.github.com>
2024-04-21 19:31:26 -0400

    2104

---
+++ vars/firstpipeline.groovy
@@ -0,0 +1,21 @@
+import com.i27academy.builds.Calculator
+def call(Map pipelineParams) {
+    Calculator calculator = new Calculator (this)
+    pipeline {
+        agent any
+        environment {
+            APP_NAME ="${pipelineParams.appname}"
+        }
+        stages {
+            stage ('Addditonal stage') {
+                steps {
+                    script {
+                        echo "printing sum of 2 numbers"
+                        println calculator.add(10,20)
+                        echo "my microservice name is:${APP_NAME}"
+                    }
+                }
+            }
+        }
+    }
+}
